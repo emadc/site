218d5d35b31218f3555462e1a0f2d9d1373c0e06
DI CARLO emanuele <emanuele.dicarlo@forcewan-lelab.fr>
2019-06-19 16:41:57 +0200

    onload

--- views/protected/protected_area.php
+++ views/protected/protected_area.php
@@ -1,1 +1,7 @@
 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+<script type="text/javascript">
+	function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+	r(function(){
+		alert("alert");
+	});
+</script>
